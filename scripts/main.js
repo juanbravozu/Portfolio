3d7fed57e6ac317239b0ea08e1277b1defa42983
Juan David Bravo <juanda.bravo98@gmail.com>
2021-04-07 13:53:51 -0500

    Added alert message about the upcoming redisgn

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,14 @@ window.addEventListener('load', () => {
     modalBehavior();
     animateSplash();
 });
+
+window.addEventListener('load', warningMessage);
+window.addEventListener('resize', warningMessage);
+
+function warningMessage(event) {
+    if(window.innerWidth < 1024) {
+        alert('Date of writing: 7th of april, 2021 \n I know this site isn\'t responsive at all but I\'ll be redesigning it soon. \n It\'ll be looking way cleaner and be also mobile friendly 🤓\n Sorry for the annoying alert')
+        event.target.removeEventListener('load', warningMessage);
+        event.target.removeEventListener('resize', warningMessage);
+    }    
+}
